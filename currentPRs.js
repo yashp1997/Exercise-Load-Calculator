497d5a24153057cea0710ef5695fa35033ca8756
Yash Patel <yash2ashok@gmail.com>
2025-11-30 19:55:56 +0530

    Create currentPRs.js

---
+++ currentPRs.js
@@ -0,0 +1,15 @@
+const currentMaxes = [
+	['squat', 'bench', 'deadlift', 'ohp', 'row'],
+	['100', '70', '140', '50', '70'],
+];
+
+//SQUAT
+console.log(currentMaxes[0][0] + " " + currentMaxes[1][0] + "KG");
+//BENCH
+console.log(currentMaxes[0][1] + " " + currentMaxes[1][1] + "KG");
+//SUMO DEADLIFT
+console.log(currentMaxes[0][2] + " " + currentMaxes[1][2] + "KG");
+//OVERHEAD PRESS
+console.log(currentMaxes[0][3] + " " + currentMaxes[1][3] + "KG");
+//BENT OVER ROW 
+console.log(currentMaxes[0][4] + " " + currentMaxes[1][4] + "KG");
